61a14d0e85eb8ab7caf931664dcc038d3308e421
shubham pandey <140872928+Shubhampandey7079@users.noreply.github.com>
2026-05-02 14:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (42.91%) - LeetSync

--- 792-binary-search/binary-search.c
+++ 792-binary-search/binary-search.c
@@ -1,16 +1,16 @@
 int search(int* nums, int numsSize, int target) {
-    int left = 0;
-    int right = numsSize - 1;
+    int low = 0;
+    int high = numsSize - 1;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
         if (nums[mid] == target) {
             return mid;
         } else if (nums[mid] < target) {
-            left = mid + 1;
+            low = mid + 1;
         } else {
-            right = mid - 1;
+            high = mid - 1;
         }
     }
 
